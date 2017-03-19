ca8fb32312114dbbcc8385c3a522458e4d476b63
Henrique Barbosa <henrikleod@gmail.com>
2017-03-19 16:22:46 -0300

    Traduçao concluida...repositório fechado

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-# Generic Variables
+# TeamVee, Henrikleod
+# não vou traduzir os comentarios, ate porque não é necessario
+# variaveis genericas
 _android="6.0.1"
 _android_version="MarshMallow"
 _custom_android="cm-13.0"
 _custom_android_version="CyanogenMod13.0"
 _github_custom_android_place="CyanogenMod"
 _github_device_place="TeamVee"
-# Make loop for usage of 'break' to recursive exit
+
+
 while true
 do
 	_unset_and_stop() {
@@ -19,8 +22,8 @@ do
 		if ! [ "${?}" == "0" ]
 		then
 			echo "  |"
-			echo "  | Something failed!"
-			echo "  | Exiting from script!"
+			echo "  | Erro desconhecido!"
+			echo "  | Fechando script!"
 			_unset_and_stop
 		fi
 	}
@@ -32,9 +35,9 @@ do
 	if [ ! "${BASH_VERSION}" ]
 	then
 		echo "  |"
-		echo "  | Please do not use 'sh' to run this script"
-		echo "  | Just use 'source build.sh'"
-		echo "  | Exiting from script!"
+		echo "  | Por favor, não use 'sh' para rodar esse script"
+		echo "  | Use o comando 'source build.sh'"
+		echo "  | Fechando script!"
 		_unset_and_stop
 	fi
 
@@ -42,10 +45,10 @@ do
 	if [ ! "$(which repo)" ]
 	then
 		echo "  |"
-		echo "  | You will need to install 'repo'"
-		echo "  | Check in this link:"
+		echo "  | Voce precisa instalar o binário 'repo'"
+		echo "  | Ele está disponivel neste link:"
 		echo "  | <https://source.android.com/source/downloading.html>"
-		echo "  | Exiting from script!"
+		echo "  | Fechando script!"
 		_unset_and_stop
 	fi
 
@@ -53,16 +56,16 @@ do
 	if [ ! "$(which curl)" ]
 	then
 		echo "  |"
-		echo "  | You will need 'curl'"
-		echo "  | Use 'sudo apt-get install curl' to install 'curl'"
-		echo "  | Exiting from script!"
+		echo "  | Voce precisa instalar o binário 'curl'"
+		echo "  | Use 'sudo apt-get install curl' para instalar"
+		echo "  | Fechando script!"
 		_unset_and_stop
 	fi
 
 	# Name of script
 	echo "  |"
-	echo "  | Live Android Sync and Build Script"
-	echo "  | For Android ${_android_version} (${_android}) | ${_custom_android_version} (${_custom_android})"
+	echo "  | Script de sincronização e compilação do Android"
+	echo "  | Para Android ${_android_version} (${_android}) | ${_custom_android_version} (${_custom_android})"
 
 	# Check option of user and transform to script
 	for _u2t in "${@}"
@@ -70,16 +73,16 @@ do
 		if [[ "${_u2t}" == "-h" || "${_u2t}" == "--help" ]]
 		then
 			echo "  |"
-			echo "  | Usage:"
-			echo "  | -h    | --help  | To show this message"
+			echo "  | Use:"
+			echo "  | -h    | --help  | Para exibir esta mensagem"
 			echo "  |"
-			echo "  | -l5   | --e610  | To build only for L5/e610"
-			echo "  | -l7   | --p700  | To build only for L7/p700"
-			echo "  | -gen1 | --gen1  | To build for L5 and L7"
+			echo "  | -l5   | --e610  | Para compilar apenas para o L5/e610"
+			echo "  | -l7   | --p700  | Para compilar apenas para o L7/p700"
+			echo "  | -gen1 | --gen1  | Para compilar para o L5 and L7"
 			echo "  |"
-			echo "  | -l1ii | --v1    | To build only for L1II/v1"
-			echo "  | -l3ii | --vee3  | To build only for L3II/vee3"
-			echo "  | -gen2 | --gen2  | To build for L1II and L3II"
+			echo "  | -l1ii | --v1    | Para compilar apenas para o L1II/v1"
+			echo "  | -l3ii | --vee3  | Para compilar apenas para o L3II/vee3"
+			echo "  | -gen2 | --gen2  | Para compilar para o L1II and L3II"
 			_option_exit="enable"
 			_unset_and_stop
 		fi
@@ -131,98 +134,98 @@ do
 
 	# Repo Sync
 	echo "  |"
-	echo "  | Starting Sync of Android Tree Manifest"
+	echo "  | Iniciando a sincronização do Android Tree Manifest"
 
 	# Device Choice
 	echo "  |"
-	echo "  | Choose Manifest to download:"
-	echo "  | 1 | First Generation Devices  | LG Optimus L5/L7 (NoNFC)"
-	echo "  | 2 | Second Generation Devices | LG Optimus L3II/L1II"
+	echo "  | Escolha o Manifest para download:"
+	echo "  | 1 | Primeira geração | LG Optimus L5/L7 (NoNFC)"
+	echo "  | 2 | Segunda geração  | LG Optimus L3II/L1II"
 	echo "  |"
 	if [ "${_device}" == "" ]
 	then
-		read -p "  | Choice | 1/ 2/ or any key to exit | " -n 1 -s x
+		read -p "  | Digite | 1/ 2/ ou qualquer outra tecla pra sair | " -n 1 -s x
 		case "${x}" in
 			1) _device="gen1";;
 			2) _device="gen2";;
-			*) echo "${x} | Exiting from script!"; _unset_and_stop;;
+			*) echo "${x} | Fechando script!"; _unset_and_stop;;
 		esac
 	fi
-	echo "  | Using ${_device}_manifest.xml"
+	echo "  | Usando ${_device}_manifest.xml"
 
 	# Remove old Manifest of Android Tree
 	echo "  |"
-	echo "  | Removing old Manifest before download new one"
+	echo "  | Removendo Manifests antigos para poder baixar um novo"
 	rm -rf .repo/manifests .repo/manifests.git .repo/manifest.xml .repo/local_manifests/
 
 	# Initialization of Android Tree
 	echo "  |"
-	echo "  | Downloading Android Tree Manifest from ${_github_custom_android_place} (${_custom_android})"
+	echo "  | Baixando Android Tree Manifest de ${_github_custom_android_place} (${_custom_android})"
 	_if_fail_break "repo init -u git://github.com/${_github_custom_android_place}/android.git -b ${_custom_android} -g all,-notdefault,-darwin"
 
 	# Device manifest download
 	echo "  |"
-	echo "  | Downloading ${_device}_manifest.xml and msm7x27a_manifest.xml"
+	echo "  | Baixando ${_device}_manifest.xml e msm7x27a_manifest.xml"
 	echo "  | From ${_github_device_place} (${_custom_android})"
 	_if_fail_break "curl -# --create-dirs -L -o .repo/local_manifests/${_device}_manifest.xml -O -L https://raw.github.com/${_github_device_place}/android_.repo_local_manifests/${_custom_android}/${_device}_manifest.xml"
 	_if_fail_break "curl -# --create-dirs -L -o .repo/local_manifests/msm7x27a_manifest.xml -O -L https://raw.github.com/${_github_device_place}/android_.repo_local_manifests/${_custom_android}/msm7x27a_manifest.xml"
 
 	# Initialize environment
 	echo "  |"
-	echo "  | Initializing the environment"
+	echo "  | Iniciando o ambiente"
 	_if_fail_break "source build/envsetup.sh"
 
 	# Real 'repo sync'
 	echo "  |"
-	echo "  | Starting Sync:"
+	echo "  | Iniciando sincronização:"
 	_if_fail_break "reposync -q --force-sync"
 
 	# Initialize environment
 	echo "  |"
-	echo "  | Initializing the environment"
+	echo "  | Iniciando o ambiente"
 	_if_fail_break "source build/envsetup.sh"
 
 	# Another device choice
 	echo "  |"
-	echo "  | For what device you want to build:"
+	echo "  | Para qual dispositivo voce quer compilar?:"
 	echo "  |"
 	if [ "${_device}" == "gen1" ]
 	then
 		echo "  | 1 | LG Optimus L5 NoNFC | E610 E612 E617"
 		echo "  | 2 | LG Optimus L7 NoNFC | P700 P705"
-		echo "  | 3 | All First Generation devices"
+		echo "  | 3 | Todos os dispositivos acima"
 		echo "  |"
 		if [ "${_device_build}" == "" ]
 		then
-			read -p "  | Choice | 1/2/3/ or * to exit | " -n 1 -s x
+			read -p "  | Digite | 1/2/3/ ou * para sair | " -n 1 -s x
 			case "${x}" in
 				1) _device_build="e610" _device_echo="L5";;
 				2) _device_build="p700" _device_echo="L7";;
 				3) _device_build="gen1" _device_echo="All Gen1";;
-				*) echo "${x} | Exiting from script!"; _unset_and_stop;;
+				*) echo "${x} | Fechando script!"; _unset_and_stop;;
 			esac
 		fi
 	elif [ "${_device}" == "gen2" ]
 	then
 		echo "  | 1 | LG Optimus L1II Single Dual | E410 E411 E415 E420"
 		echo "  | 2 | LG Optimus L3II Single Dual | E425 E430 E431 E435"
-		echo "  | 3 | All Second Generation devices"
+		echo "  | 3 | Todos os dispositivos acima"
 		echo "  |"
 		if [ "${_device_build}" == "" ]
 		then
-			read -p "  | Choice | 1/2/3/ or * to exit | " -n 1 -s x
+			read -p "  | Digite | 1/2/3/ ou * para sair| " -n 1 -s x
 			case "${x}" in
 				1) _device_build="v1" _device_echo="L1II";;
 				2) _device_build="vee3" _device_echo="L3II";;
 				3) _device_build="gen2" _device_echo="All Gen2";;
-				*) echo "${x} | Exiting from script!"; _unset_and_stop;;
+				*) echo "${x} | Fechando script!"; _unset_and_stop;;
 			esac
 		fi
 	fi
-	echo "  | Building to ${_device_echo}"
+	echo "  | Compilando para o ${_device_echo}"
 	# Builing Android
 	echo "  |"
-	echo "  | Starting Android Building!"
+	echo "  | Iniciando compilação!"
 	if [[ "${_device_build}" == "e610" || "${_device_build}" == "gen1" ]]
 	then
 		_if_fail_break "brunch e610"
@@ -246,4 +249,4 @@ done
 
 # Goodbye!
 echo "  |"
-echo "  | Thanks for using this script!"
+echo "  | Obrigado por usar este script!"
